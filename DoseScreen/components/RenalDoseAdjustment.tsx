d3387dfee052716a05a23dee92328ad75ae8de37
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-10-06 13:19:57 +0200

    added GFR calculations

--- DoseScreen/components/RenalDoseAdjustment.tsx
+++ DoseScreen/components/RenalDoseAdjustment.tsx
@@ -1,56 +1,102 @@
 import { View, StyleSheet, Text } from "react-native";
-import { FormControl, Input, Select, CheckIcon } from "native-base";
-import { useState } from "react";
+import { FormControl, Input, Select, CheckIcon, Button } from "native-base";
+import { useEffect, useState } from "react";
 import theme from "../../theme";
 
 export default function RenalDoseAdjustment() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [gender, setGender] = useState("");
+  const [age, setAge] = useState(0);
+  const [scr, setScr] = useState(0);
+  const [isFormFilled, setIsFormFilled] = useState(false);
+  const [calculatedGFR, setCalculatedGFR] = useState(0);
+
+  const updateState = (val, setter) => {
+    setter(val);
+  };
+
+  const calculateGFR = () => {
+    const factor = gender === "m" ? 1 : 0.85;
+    const GFR = ((140 - age) * weight * factor) / (scr * 72);
+    setCalculatedGFR(GFR);
+  };
+
+  useEffect(() => {
+    if (weight > 0 && height > 0 && gender.length === 1 && age > 0 && scr > 0) {
+      setIsFormFilled(true);
+    } else {
+      setIsFormFilled(false);
+    }
+  }, [weight, height, gender, age, scr]);
+
   return (
-    <FormControl isRequired>
-      <View style={styles.inputGroup}>
-        <Text>Weight: </Text>
-        <Input
-          w={"75%"}
-          keyboardType="number-pad"
-          InputRightElement={<Text style={styles.inputTextElement}>kg</Text>}
-        />
-      </View>
-      <View style={styles.inputGroup}>
-        <Text>Height: </Text>
-        <Input
-          w={"75%"}
-          keyboardType="number-pad"
-          InputRightElement={<Text style={styles.inputTextElement}>cm</Text>}
-        />
-      </View>
-      <View style={styles.inputGroup}>
-        <Text>Gender: </Text>
-        <Select
-          minWidth="200"
-          accessibilityLabel="Choose gender"
-          placeholder="Gender"
-          _selectedItem={{
-            bg: "teal.600",
-            endIcon: <CheckIcon size={2} />,
-          }}
-          w={"75%"}
+    <>
+      <FormControl isRequired>
+        <View style={styles.inputGroup}>
+          <Text>Weight: </Text>
+          <Input
+            w={"75%"}
+            keyboardType="number-pad"
+            InputRightElement={<Text style={styles.inputTextElement}>kg</Text>}
+            onChangeText={(val) => updateState(val, setWeight)}
+          />
+        </View>
+        <View style={styles.inputGroup}>
+          <Text>Height: </Text>
+          <Input
+            w={"75%"}
+            keyboardType="number-pad"
+            InputRightElement={<Text style={styles.inputTextElement}>cm</Text>}
+            onChangeText={(val) => updateState(val, setHeight)}
+          />
+        </View>
+        <View style={styles.inputGroup}>
+          <Text>Gender: </Text>
+          <Select
+            minWidth="200"
+            accessibilityLabel="Choose gender"
+            placeholder="Gender"
+            _selectedItem={{
+              bg: "teal.600",
+              endIcon: <CheckIcon size={2} />,
+            }}
+            w={"75%"}
+            onValueChange={(val) => updateState(val, setGender)}
+          >
+            <Select.Item label="Male" value="m" />
+            <Select.Item label="Female" value="f" />
+          </Select>
+        </View>
+        <View style={styles.inputGroup}>
+          <Text>Age: </Text>
+          <Input
+            w={"75%"}
+            keyboardType="number-pad"
+            onChangeText={(val) => updateState(val, setAge)}
+          />
+        </View>
+        <View style={styles.inputGroup}>
+          <Text>S. Cr: </Text>
+          <Input
+            w={"75%"}
+            keyboardType="number-pad"
+            InputRightElement={
+              <Text style={styles.inputTextElement}>mg/dL</Text>
+            }
+            onChangeText={(val) => updateState(val, setScr)}
+          />
+        </View>
+        <Button
+          colorScheme="red"
+          isDisabled={!isFormFilled}
+          onPress={() => calculateGFR()}
         >
-          <Select.Item label="Male" value="m" />
-          <Select.Item label="Female" value="f" />
-        </Select>
-      </View>
-      <View style={styles.inputGroup}>
-        <Text>Age: </Text>
-        <Input w={"75%"} keyboardType="number-pad" />
-      </View>
-      <View style={styles.inputGroup}>
-        <Text>S. Cr: </Text>
-        <Input
-          w={"75%"}
-          keyboardType="number-pad"
-          InputRightElement={<Text style={styles.inputTextElement}>mg/dL</Text>}
-        />
-      </View>
-    </FormControl>
+          Calculate Dose
+        </Button>
+      </FormControl>
+      {calculatedGFR && <Text>GFR = {calculatedGFR}</Text>}
+    </>
   );
 }
 
