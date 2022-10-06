97d0d2f2b56fed32abad3529b887c056b96646dd
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-10-06 17:56:15 +0200

    finished hemodialysis screen

--- HemodialysisScreen.tsx
+++ HemodialysisScreen.tsx
@@ -1,22 +1,56 @@
+import { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import drugs from "./drugs";
 import theme from "./theme";
 
 export default function HemodialysisScreen() {
+  const [drugsSorted, setDrugsSorted] = useState({
+    compatible: [],
+    incompatible: [],
+  });
   const hemodialysisCompatibleDrugs = [];
   const hemodialysisInCompatibleDrugs = [];
 
+  useEffect(() => {
+    drugs.forEach((drug) => {
+      if (drug.dialysis) {
+        hemodialysisCompatibleDrugs.push({ name: drug.name, id: drug.id });
+      } else {
+        hemodialysisInCompatibleDrugs.push({ name: drug.name, id: drug.id });
+      }
+    });
+    setDrugsSorted({
+      compatible: hemodialysisCompatibleDrugs,
+      incompatible: hemodialysisInCompatibleDrugs,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>Compatible with hemodialysis</Text>
-      <View></View>
-      <Text>Contraindicated with hemodialysis</Text>
-      <View></View>
+    <View style={styles.container}>
+      <Text style={styles.header}>Compatible with hemodialysis</Text>
+      <View>
+        {Object.keys(drugsSorted).length !== 0
+          ? drugsSorted.compatible.map((drug, index) => {
+              return <Text key={index}>{drug.name}</Text>;
+            })
+          : null}
+      </View>
+      <Text style={styles.header}>Contraindicated with hemodialysis</Text>
+      <View>
+        {Object.keys(drugsSorted).length !== 0
+          ? drugsSorted.incompatible.map((drug, index) => {
+              return <Text key={index}>{drug.name}</Text>;
+            })
+          : null}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+  },
   header: {
     fontSize: theme.FONT_SIZE_LARGE,
     marginBottom: 15,
