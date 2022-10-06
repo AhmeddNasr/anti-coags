3aa08c87b080f06dad87fa3f55a8cc607ba33092
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-10-06 11:52:46 +0200

    added indication and adjustment type radio inputs

--- DrugDoseScreen.tsx
+++ DrugDoseScreen.tsx
@@ -1,12 +1,75 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet, ScrollView } from "react-native";
+import { useState } from "react";
+import { Radio } from "native-base";
 import drugs from "./drugs";
+import theme from "./theme";
+import RenalDoseAdjustment from "./DoseScreen/components/RenalDoseAdjustment";
 
 export default function DrugDoseScreen({ navigation, route }) {
+  const [adjustment, setAdjustment] = useState("");
+  const [indication, setIndication] = useState("");
   const drug = drugs[route.params.id];
 
   return (
-    <View>
-      <Text>{drug.name}</Text>
+    <View style={{ margin: 20 }}>
+      <ScrollView>
+        {/* Indication */}
+        <Text style={[styles.header, { marginTop: 0 }]}>1. Indication: </Text>
+        <Radio.Group
+          name="indication"
+          accessibilityLabel="indication"
+          value={indication}
+          onChange={(nextValue) => setIndication(nextValue)}
+        >
+          {drug.indications.map((indication, index) => {
+            return (
+              <Radio colorScheme="red" value={indication} key={index} my={2}>
+                {indication === "dvtp"
+                  ? "DVT prophylaxis"
+                  : indication === "dvtt"
+                  ? "DVT treatment"
+                  : "AF"}
+              </Radio>
+            );
+          })}
+        </Radio.Group>
+
+        {/* Adjustment Type */}
+        {indication.length > 0 && (
+          <>
+            <Text style={styles.header}>2. Adjustment Type:</Text>
+            <Radio.Group
+              name="adjustmentType"
+              accessibilityLabel="adjustment type"
+              value={adjustment}
+              onChange={(nextValue) => setAdjustment(nextValue)}
+            >
+              <Radio value="renal" colorScheme="red" my={2}>
+                Renal Dose Adjustment
+              </Radio>
+              <Radio value="hepatic" colorScheme="red" my={2}>
+                Hepatic Dose Adjustment
+              </Radio>
+            </Radio.Group>
+          </>
+        )}
+        {adjustment.length > 0 && (
+          <Text style={styles.header}>
+            {adjustment === "renal"
+              ? "3. Calculate GFR"
+              : "4. Calculate CHILD score"}
+          </Text>
+        )}
+        {adjustment === "renal" ? <RenalDoseAdjustment /> : null}
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 18,
+    marginBottom: 10,
+    marginTop: 20,
+  },
+});
