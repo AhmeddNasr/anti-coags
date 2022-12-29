b71c72fd1c6c708a52d90a1aa57bd1faed6fe01f
Ahmed Nasr <ahmeddhemd@gmail.com>
2022-12-29 05:55:34 +0200

    separate gfr function

---
+++ Utils/calculateGFR.js
@@ -0,0 +1,5 @@
+export default function calculateGFR(gender, age, weight, scr) {
+  const factor = gender === "m" ? 1 : 0.85;
+  const GFR = ((140 - age) * weight * factor) / (scr * 72);
+  return GFR;
+}
