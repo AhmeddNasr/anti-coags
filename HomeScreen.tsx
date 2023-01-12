92e9078a7034fe81b916d7ccc5a7476bef4d8b34
Ahmed Nasr <ahmeddhemd@gmail.com>
2023-01-12 12:44:38 +0200

    redesign homescreen

--- HomeScreen.tsx
+++ HomeScreen.tsx
@@ -3,45 +3,164 @@ import {
   View,
   StyleSheet,
   TouchableOpacity,
-  ScrollView,
+  FlatList,
+  Pressable,
+  StatusBar,
+  Image,
 } from "react-native";
-import { Button } from "native-base";
+// import { LinearGradient } from "expo-linear-gradient";
+import * as Icon from "@expo/vector-icons";
 
 export default function HomeScreen({ navigation }) {
   const CustomButton = (props) => {
+    if (!props.title) {
+      return <View style={{ flex: 1, margin: 20 }}></View>;
+    }
     return (
-      <Button
+      <Pressable
         colorScheme="red"
         onPress={() => navigation.navigate(props.screen)}
-        my={5}
-        // w={"75%"}
-        py={4}
+        style={({ pressed }) => [
+          styles.button,
+          pressed
+            ? {
+                backgroundColor: "#5BC0F8",
+              }
+            : null,
+        ]}
       >
-        {props.title}
-      </Button>
+        <Icon.Fontisto name={props.icon} color="white" size={40} />
+        <Text style={{ fontSize: 17, marginTop: 10, color: "white" }}>
+          {props.title}
+        </Text>
+        <Text
+          style={{
+            color: "white",
+            alignSelf: "flex-start",
+            fontSize: 13,
+            fontStyle: "italic",
+          }}
+        >
+          {props.description}
+        </Text>
+      </Pressable>
+    );
+  };
+
+  const data = [
+    [
+      {
+        title: "Calculate Dose",
+        screen: "Dose",
+        bg: "#3a86ff",
+        icon: "calculator",
+        description: "Adjust dose according to patient's data",
+      },
+      {
+        title: "Hemodialysis",
+        screen: "Hemodialysis",
+        bg: "#ff006e",
+        icon: "blood-drop",
+        description: "Hemodialysis compatibility guide",
+      },
+    ],
+    [
+      {
+        title: "Switching",
+        screen: "Switching",
+        bg: "#fb5607",
+        icon: "arrow-swap",
+        description: "Switching to another agent guide",
+      },
+      {
+        title: "Choosing an anticoagulant",
+        screen: "Choosing",
+        bg: "#8338ec",
+        icon: "search",
+        description: "Choosing a suitable anticoagulant guide",
+      },
+    ],
+    [
+      {
+        title: "Warnings",
+        screen: "Warning",
+        bg: "#3a86ff",
+        icon: "travis",
+        description: "Warnings and precautions for the use of anticoagulants",
+      },
+      {},
+    ],
+  ];
+
+  const renderItem = ({ item }) => {
+    return (
+      <CustomButton
+        title={item.title}
+        screen={item.screen}
+        bg={item.bg}
+        icon={item.icon}
+      />
     );
   };
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <CustomButton title="Dose Modification" screen="Dose" />
-        <CustomButton title="Hemodialysis" screen="Hemodialysis" />
-        <CustomButton title="Switching" screen="Switching" />
-        <CustomButton
-          title="Choose a Suitable Anti-coagulant"
-          screen="Choosing"
-        />
-        <CustomButton title="Special Warnings" screen="Warning" />
-      </ScrollView>
+      {data.map((group, index) => {
+        return (
+          <View key={index} style={styles.group}>
+            {group.map((item) => {
+              return (
+                <CustomButton
+                  title={item.title}
+                  screen={item.screen}
+                  bg={item.bg}
+                  icon={item.icon}
+                  description={item.description}
+                />
+              );
+            })}
+          </View>
+        );
+      })}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
+    // justifyContent: "center",
+    // alignItems: "stretch",
+    flex: 1,
+    // backgroundColor: "pink",
+    // flexDirection: "row",
+    // backgroundColor: "#203453",
+    margin: 30,
+  },
+  group: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "stretch",
+    justifyContent: "space-evenly",
+    // backgroundColor: "red",
+  },
+  button: {
+    margin: 10,
+    // width: 100,
+    // height: 100,
+    borderRadius: 15,
+    // borderWidth: 1,
+    paddingTop: 20,
+    padding: 10,
+    flex: 1,
+    // flexGrow: 1,
+    // backgroundColor: props.bg,
+    // alignContent: "center",
+    // justifyContent: "center",
     alignItems: "center",
-    marginTop: 30,
+    backgroundColor: "#E84545",
+    justifyContent: "space-around",
+  },
+  text: {
+    color: "white",
   },
 });
