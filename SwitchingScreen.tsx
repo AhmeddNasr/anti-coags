ab7cd6f0ff3f533b78174556552fd24e148ce863
Ahmed Nasr <ahmeddhemd@gmail.com>
2023-01-25 16:20:06 +0200

    Update SwitchingScreen.tsx

--- SwitchingScreen.tsx
+++ SwitchingScreen.tsx
@@ -1,5 +1,5 @@
 import { Select, CheckIcon, Button } from "native-base";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { useState } from "react";
 import drugs from "./drugs";
 
@@ -19,18 +19,23 @@ export default function SwitchingScreen() {
         }}
         w={"75%"}
         mt={10}
+        onValueChange={(value) => {
+          if (value === secondDrug) {
+            setSecondDrug("");
+          }
+          setFirstDrug(value);
+        }}
       >
         {drugs.map((drug, key) => {
           return (
             <Select.Item
               label={drug.name}
-              key={key}
-              value={drug.id.toString()}
+              key={`Switch1-${key}`}
+              value={drug.name}
             />
           );
         })}
       </Select>
-
       <Select
         minWidth="200"
         accessibilityLabel="Second drug"
@@ -41,19 +46,39 @@ export default function SwitchingScreen() {
         }}
         w={"75%"}
         mt={10}
+        isDisabled={firstDrug === ""}
+        onValueChange={(value) => setSecondDrug(value)}
       >
         {drugs.map((drug, key) => {
-          return (
-            <Select.Item
-              label={drug.name}
-              key={key}
-              value={drug.id.toString()}
-            />
-          );
+          if (drug.name === firstDrug) {
+            return null;
+          }
+          return <Select.Item label={drug.name} key={key} value={drug.name} />;
         })}
       </Select>
+      <Text style={{ marginTop: 30 }}>first drug: {firstDrug}</Text>
+      <Text style={{ marginTop: 0 }}>second drug: {secondDrug}</Text>
+
+      {firstDrug !== "" && secondDrug !== "" && (
+        <Results firstDrug={firstDrug} secondDrug={secondDrug} />
+      )}
     </View>
   );
+}
+
+function Results(props) {
+  const [drug1Switching, setDrug1Switching] = useState({});
+  const [drug2Name, setDrug2Name] = useState("");
+  const [results, setResults] = useState([]);
+  for (let i = 0; i < drugs.length; i++) {
+    if (props.firstDrug === drugs[i].name) {
+      setDrug1Switching(drugs[i].switching);
+    } else if (props.secondDrug === drugs[i].name) {
+      setDrug2Name(drugs[i].name);
+    }
+  }
+
+  return null;
 }
 
 const styles = StyleSheet.create({
