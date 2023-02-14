63b10565f0e753d4f94624bf0991697fd22626b0
Ahmed Nasr <ahmeddhemd@gmail.com>
2023-02-14 05:13:15 +0200

    add fondaparinux

--- drugs.js
+++ drugs.js
@@ -4,7 +4,16 @@ import apixaban from "./drugs/apixaban";
 import heparin from "./drugs/heparin";
 import enoxaparin from "./drugs/enoxaparin";
 import warfarin from "./drugs/warfarin";
+import fondaparinux from "./drugs/fondaparinux";
 
-const drugs = [rivaroxaban, edoxaban, apixaban, enoxaparin, heparin, warfarin];
+const drugs = [
+  rivaroxaban,
+  edoxaban,
+  apixaban,
+  enoxaparin,
+  heparin,
+  warfarin,
+  fondaparinux,
+];
 
 export default drugs;
