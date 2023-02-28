bf4cb0481d9776520d10ff08b723223d8fb2c1cf
Ahmed Nasr <ahmeddhemd@gmail.com>
2023-02-28 21:45:08 +0200

    add custom navbar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text } from "react-native";
+import { Text, View, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import DoseScreen from "./DoseScreen";
@@ -12,10 +12,6 @@ import { useFonts } from "expo-font";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as Icon from "@expo/vector-icons";
 //TODO
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from "react-native-safe-area-context";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import HomeScreen from "./HomeScreen";
 import capitalizeFirstLetter from "./Utils/capitalizeFirstLetter";
@@ -35,92 +31,51 @@ export default function App() {
     style: [{ fontFamily: "Proxima-Nova" }],
   });
 
-  const Tab = createBottomTabNavigator();
-
   return (
     <NativeBaseProvider>
       <NavigationContainer>
-        {/* <Tab.Navigator
-          initialRouteName="Dose"
-          safeAreaInsets={{ top: 200 }}
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              if (route.name === "Dose") {
-                iconName = focused
-                  ? "ios-calculator"
-                  : "ios-calculator-outline";
-              } else if (route.name === "Checklist") {
-                iconName = focused
-                  ? "ios-search-circle"
-                  : "ios-search-circle-outline";
-              } else if (route.name === "Switching") {
-                iconName = focused ? "sync-circle" : "sync-circle-outline";
-              }
-              return (
-                <Icon.Ionicons name={iconName} color={color} size={size} />
-              );
-            },
-            tabBarLabel:
-              route.name === "Dose"
-                ? "Dose Modification"
-                : route.name === "Switching"
-                ? "Switching"
-                : "Checklist",
-            tabBarHideOnKeyboard: true,
-            tabBarActiveTintColor: "#fff",
-            tabBarInactiveTintColor: "#ffffff90",
-            headerTitleAlign: "center",
-            tabBarStyle: {
-              backgroundColor: myTheme.default.BLUE_COLOR,
-              height: 70,
-              paddingBottom: 10,
-              paddingTop: 10,
-              borderWidth: 0,
-            },
-            // headerStyle: {
-            //   backgroundColor: myTheme.default.BACKGROUND_COLOR,
-            // },
-
-            headerTitleStyle: {
-              // color: "white",
-              fontWeight: "600",
-              color: myTheme.default.PRIMARY_COLOR,
-              fontFamily: "inter-font",
-            },
-
-            headerShadowVisible: false,
-          })}
-        >
-          <Tab.Screen name="Checklist" component={ChoosingScreen} />
-          <Tab.Screen name="Dose" component={DoseScreen} />
-          <Tab.Screen name="Switching" component={SwitchingScreen} />
-        </Tab.Navigator> */}
-
         <Stack.Navigator
           screenOptions={{
             headerTitleAlign: "center",
-            headerStyle: {
-              backgroundColor: myTheme.default.BACKGROUND_COLOR,
-            },
+            // headerStyle: {
+            //   backgroundColor: myTheme.default.DARK_RED,
+            // },
             headerTitleStyle: {
               // color: "white",
               fontWeight: "700",
-              color: myTheme.default.PRIMARY_COLOR,
+              color: "white",
               fontFamily: "inter-font",
             },
             headerShadowVisible: false,
             contentStyle: {
               backgroundColor: myTheme.default.BACKGROUND_COLOR,
             },
+            header: ({ route, navigation, back, options }) => (
+              <View
+                style={{
+                  // height: 100,
+
+                  paddingTop: StatusBar.currentHeight + 20,
+                  paddingBottom: 40,
+                  backgroundColor: myTheme.default.DARK_RED,
+                  // justifyContent: "center",
+                  alignItems: "center",
+                  borderBottomRightRadius: 40,
+                  borderBottomLeftRadius: 40,
+                }}
+              >
+                <Text style={{ color: "white", fontSize: 30 }}>
+                  {options.title}
+                </Text>
+              </View>
+            ),
           }}
         >
           <Stack.Screen
             name="Home"
             component={HomeScreen}
             options={{
-              title: "",
-              headerShown: false,
+              title: "CoaguRX",
             }}
           />
           <Stack.Screen
