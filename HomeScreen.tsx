0801390121cfa08b4c72beaaf7b82894f432db97
Ahmed Nasr <ahmeddhemd@gmail.com>
2023-02-28 21:47:51 +0200

    update styling

--- HomeScreen.tsx
+++ HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, Pressable } from "react-native";
+import { Text, View, StyleSheet, Pressable, Image } from "react-native";
 import * as Icon from "@expo/vector-icons";
 import theme from "./theme";
 
@@ -7,27 +7,23 @@ export default function HomeScreen({ navigation }) {
     {
       title: "Dosing",
       screen: "Dose",
-      bg: "#3a86ff",
-      icon: "ios-calculator",
       description:
         "Calculate optimum dose based on patient's weight, height, renal and hepatic function",
     },
     {
       title: "Switching",
       screen: "Switching",
-      bg: "#fb5607",
-      icon: "sync-circle-outline",
       description: "Switching to another anticoagulant agent guideline",
     },
     {
       title: "Checklist",
       screen: "Choosing",
-      bg: "#8338ec",
-      icon: "ios-search-circle-outline",
       description: "Find suitable anti-coagulants based on patient's data",
     },
   ];
-
+  let checkIcon = require("./assets/icons/checklist-checked-box.png");
+  let switchIcon = require("./assets/icons/swap.png");
+  let doseIcon = require("./assets/icons/balance-sheet.png");
   function CustomButton(props: any) {
     return (
       <Pressable
@@ -42,23 +38,54 @@ export default function HomeScreen({ navigation }) {
           { justifyContent: "space-between" },
         ]}
       >
-        <View>
-          <Text
-            style={[
-              styles.text,
-              {
-                fontSize: theme.FONT_SIZE_EXTRA_LARGE,
-                fontFamily: "inter-font",
-                fontWeight: "900",
-                marginTop: 10,
-              },
-            ]}
+        <View style={{ justifyContent: "center", alignItems: "center" }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              // backgroundColor: "cyan",
+              padding: 10,
+            }}
           >
-            {props.title}
-          </Text>
+            <Image
+              source={
+                props.title === "Dosing"
+                  ? doseIcon
+                  : props.title === "Switching"
+                  ? switchIcon
+                  : checkIcon
+              }
+              style={{
+                width: 25,
+                height: 25,
+                // alignSelf: "center",
+                marginRight: 8,
+                // backgroundColor: "blue",
+              }}
+            />
+            <Text
+              style={[
+                styles.text,
+                {
+                  fontSize: 25,
+                  fontFamily: "inter-font",
+                  fontWeight: "900",
+                  // marginTop: 10,
+                  // padding: 10,
+                  paddingRight: 0,
+                  paddingLeft: 0,
+                  // backgroundColor: "blue",
+                  // alignSelf: "center",
+                },
+              ]}
+            >
+              {props.title}
+            </Text>
+          </View>
           <Text
             style={{
-              color: theme.TEXT_COLOR_WHITE,
+              color: theme.LIGHT_BLACK,
               alignSelf: "flex-start",
               fontSize: 14,
               fontStyle: "italic",
@@ -67,35 +94,31 @@ export default function HomeScreen({ navigation }) {
             {props.description}
           </Text>
         </View>
-        <Icon.Ionicons
-          name={props.icon}
-          color={theme.TEXT_COLOR_WHITE}
-          size={45}
-          style={{ alignSelf: "flex-end", margin: 10 }}
-        />
       </Pressable>
     );
   }
 
   return (
     <View style={styles.container}>
-      <Text
-        style={{
-          marginBottom: 30,
-          marginTop: 30,
-          fontSize: theme.FONT_SIZE_LOGO,
-          fontFamily: "inter-font",
-        }}
-      >
-        CoaguRx
-      </Text>
+      {/* <View>
+        <Text
+          style={{
+            marginBottom: 30,
+            marginTop: 30,
+            fontSize: theme.FONT_SIZE_LOGO,
+            fontFamily: "inter-font",
+            alignSelf: "center",
+            color: "white",
+          }}
+        >
+          CoaguRx
+        </Text>
+      </View> */}
       {data.map((item, index) => {
         return (
           <CustomButton
             title={item.title}
             screen={item.screen}
-            bg={item.bg}
-            icon={item.icon}
             description={item.description}
             key={`homeButton-${index}`}
           />
@@ -107,42 +130,20 @@ export default function HomeScreen({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
-    // justifyContent: "center",
-    // alignItems: "stretch",
     flex: 1,
-    // backgroundColor: "pink",
-    // flexDirection: "row",
-    // backgroundColor: "#203453",
+
     margin: 30,
   },
-  // group: {
-  //   flexDirection: "row",
-  //   flex: 1,
-  //   alignItems: "stretch",
-  //   justifyContent: "space-evenly",
-  //   // backgroundColor: "red",
-  // },
+
   button: {
-    // margin: 10,
-    // width: 100,
-    // height: 100,
     borderRadius: 15,
-    // borderWidth: 1,
-    // paddingTop: 20,
     padding: 10,
     flex: 1,
-    // borderColor: theme.TEXT_COLOR_GRAY,
-    backgroundColor: theme.BLUE_COLOR,
+    backgroundColor: theme.LIGHT_GREY,
     marginBottom: 30,
-    // flexGrow: 1,
-    // backgroundColor: props.bg,
-    // alignContent: "center",
-    // justifyContent: "center",
-    // alignItems: "center",
-    // backgroundColor: "#E84545",
-    // justifyContent: "space-around",
+    maxHeight: 150,
   },
   text: {
-    color: "white",
+    color: "BLACK",
   },
 });
